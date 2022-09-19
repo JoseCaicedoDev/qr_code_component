680775657d8666c25335ab6b5c969954edee1e95
jcaicedo <jose.caicedo@outlook.com>
2022-09-19 10:57:21 -0500

    feat(configuración favicon)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import imageQr from './components/assets/image-qr-code.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <div className='card'>
+          <img className='card__image' src={imageQr} alt="" />
+          <h1 className='card__title'>Improve your front-end skills bu building projects</h1>
+          <p className='card__description'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </section>
+    </>
   );
 }
 
